478b711f2b3ca5ffd0bedffc07b47e7873964d32
Oscar <a076437710@gmail.com>
2025-04-20 20:40:06 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+//import router from './router'
 import '@/assets/css/header.css'
 import '@/assets/css/main.css'
 import '@/assets/css/footer.css'
 
 const app = createApp(App)
 
-app.use(router)
+//app.use(router)
 
 app.mount('#app')
