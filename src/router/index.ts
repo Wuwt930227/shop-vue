9d7acab91a552423bebfa54b64be6e483dbd3f89
Oscar <a076437710@gmail.com>
2025-04-20 20:38:36 +0800

    index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router'
+/*import { createRouter, createWebHistory } from 'vue-router'
+import Home from ""
+
+const routes = [
+  {
+    path: '/',
+    name:"",
+    component: Home
+  },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-  ],
+  history: createWebHistory(),
+  routes
 })
 
 export default router
+需要時再取消註解*/
