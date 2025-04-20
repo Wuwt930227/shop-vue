c321c88d36685407cdcf341ee30497946f362513
Oscar <a076437710@gmail.com>
2025-04-20 18:46:57 +0800

    update

--- src/components/main/main-one.vue
+++ src/components/main/main-one.vue
@@ -4,7 +4,7 @@
             <button class="arrow-button arrow-left"><i class="fa-solid fa-arrow-left"></i></button>
             <img :src="one" alt="圖片">
             <button class="arrow-button arrow-right"><i class="fa-solid fa-arrow-right"></i></button>
-            </div>
+        </div>
         <div class="a">
             <div class="g">
                 <img :src="two" width="100%" height="100%">
