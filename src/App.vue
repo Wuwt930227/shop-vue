83d1b9e8e4a1c19eaba72ab1e6698edc2b6a8107
evan940629 <lin940629@gmail.com>
2025-04-20 18:40:32 +0800

    main

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-    import one from '@/assets/image/ONE.png'
-    import two from '@/assets/image/TWO.png'
-    import three from '@/assets/image/THREE.png'
     import down from '@/assets/image/down.png'
     import headertop from '@/components/header/header-top.vue'
     import headermainnav from '@/components/header/header-main-nav.vue'
     import headercategories from '@/components/header/header-categories.vue'
+    import mainone from '@/components/main/main-one.vue'
 </script>
 
 <template>
@@ -16,21 +14,7 @@
     </header>
     <br>
     <main>
-        <div class="two">
-            <div class="image-container">
-                <button class="arrow-button arrow-left"><i class="fa-solid fa-arrow-left"></i></button>
-                <img :src="one" alt="圖片">
-                <button class="arrow-button arrow-right"><i class="fa-solid fa-arrow-right"></i></button>
-              </div>
-            <div class="a">
-                <div class="g">
-                    <img :src="two" width="100%" height="100%">
-                </div>
-                <div class="g">
-                    <img :src="three" width="100%" height="100%">
-                </div>
-            </div>
-        </div>
+        <mainone/>
     </main>
         <br>
     <footer>
---
+++ src/components/main/main-one.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="two">
+        <div class="image-container">
+            <button class="arrow-button arrow-left"><i class="fa-solid fa-arrow-left"></i></button>
+            <img :src="one" alt="圖片">
+            <button class="arrow-button arrow-right"><i class="fa-solid fa-arrow-right"></i></button>
+            </div>
+        <div class="a">
+            <div class="g">
+                <img :src="two" width="100%" height="100%">
+            </div>
+            <div class="g">
+                <img :src="three" width="100%" height="100%">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import one from '@/assets/image/ONE.png'
+    import two from '@/assets/image/TWO.png'
+    import three from '@/assets/image/THREE.png'
+</script>
+
+<style scoped>
+</style>
