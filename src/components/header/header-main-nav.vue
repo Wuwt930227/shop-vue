a91cf1efe6b5882038d60f0948d49da7471dc8bd
Oscar <a076437710@gmail.com>
2025-04-20 22:40:29 +0800

    cheap-phone

--- src/components/header/header-main-nav.vue
+++ src/components/header/header-main-nav.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="main-nav">
-        <div class="logo"><i class="fa-solid fa-bag-shopping"></i> 蝦皮購物</div>
+        <div class="logo"><router-link to="/"><i class="fa-solid fa-bag-shopping"></i> 蝦皮購物</router-link></div>
         <div class="search-bar">
             <input type="text" placeholder="註冊獲得全站優惠券與免運券">
             <button><i class=" fa-solid fa-magnifying-glass"></i></button>
