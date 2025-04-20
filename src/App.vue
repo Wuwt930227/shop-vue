eb62521cfbcd5b5126b2a48816acd8b1af55bc92
Oscar <a076437710@gmail.com>
2025-04-20 23:37:57 +0800

    復原man

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
     import down from '@/assets/image/down.png'
+    import mainone from '@/components/main/main-one.vue'
+
 </script>
 
 <template>
     <router-view name="header"></router-view>
     <br>
-    <router-view name="main"></router-view>
+    <!--<router-view name="main"></router-view>-->
+    <mainone></mainone>
     <br>
     <footer>
         <div class="three">
